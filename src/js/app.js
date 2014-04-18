63e413237b053ba3a59f4ce26a0fcfc385385091
Dem Pilafian <dem@Lightning-2.local>
2014-04-17 22:09:33 -0700

    Restore gallery folder

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-// PPAGES ~ www.centerkey.com/ppages ~ Copyright (c) individual contributors
-// Rights granted under GNU General Public License ~ ppages/src/gallery/license.txt
-
-$(function() {
-   displaySocialButtons($('#social-buttons'));
-   setupLinks();
-   });
