414299750931b3e1fe23a8848c467c965bfed125
Dem Pilafian <pilaf@centerkey.com>
2014-10-02 21:35:33 -0700

    Anonymous init function replaced with named function

--- src/gallery/console/js/start.js
+++ src/gallery/console/js/start.js
@@ -5,14 +5,21 @@
 
 // Application
 
-$(function() {
-   library.social.setup();
-   library.ui.setup();
-   library.form.setup();
-   library.bubbleHelp.setup();
-   gmc.ui.setupLinks();
-   gmc.ui.setupActions();
-   gmc.ui.setupStatusMsg();
-   gmc.ui.autoFocus($('.login'));
-   gmc.ui.configureMenuBarButtons();
-   });
+if (!gmc)
+   var gmc = {};
+
+gmc.start = {
+   go: function() {
+      library.social.setup();
+      library.ui.setup();
+      library.form.setup();
+      library.bubbleHelp.setup();
+      gmc.ui.setupLinks();
+      gmc.ui.setupActions();
+      gmc.ui.setupStatusMsg();
+      gmc.ui.autoFocus($('.login'));
+      gmc.ui.configureMenuBarButtons();
+      }
+   };
+
+$(gmc.start.go);
