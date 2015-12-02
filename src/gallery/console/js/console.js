4168c8f3e20771e9780ee5fb62fae2112047df87
Dem Pilafian <dem@centerkey.com>
2015-12-01 22:18:07 -0800

    Replaced "this" with specific module name

--- src/gallery/console/js/console.js
+++ src/gallery/console/js/console.js
@@ -26,29 +26,29 @@ gmc.ui = {
       elem.stop().fadeTo('fast', 1.0).fadeTo(delayVerySlow, 0.4);
       },
    statusMsg: function(msg) {
-      this.slowDimOut($('#status-msg').html(msg));
+      gmc.ui.slowDimOut($('#status-msg').html(msg));
       },
    setupStatusMsg: function() {
       var msg = '' + $('#status-msg').html();
       if (msg.length === 0)
          msg = 'Ready';
-      this.statusMsg(msg);
+      gmc.ui.statusMsg(msg);
       },
    webServiceUrl: function(params) {
       var baseUrl = 'service';
       var url = baseUrl;
-      function buildUrl(param) {
-         if (params[param] !== undefined)
+      function buildUrl(key) {
+         if (params[key] !== undefined)
             url += (url === baseUrl ? '?' : '&') +
-               param + '=' + encodeURIComponent(params[param]);
+               key + '=' + encodeURIComponent(params[key]);
          }
-      params.forEach(buildUrl);
+      Object.keys(params).forEach(buildUrl);
       return url;
       },
    processServiceResponce: function(data, successMsg) {
       if (data.error === gmc.error.codeAuthFail)
          window.location.reload();
-      this.statusMsg(data.error ? 'Error: ' + data.message : successMsg);
+      gmc.ui.statusMsg(data.error ? 'Error: ' + data.message : successMsg);
       return !data.error;
       },
    updatePortfolio: function(id, field, value) {
@@ -58,8 +58,11 @@ gmc.ui = {
          field:  field,
          value:  value
          };
-      $.getJSON(this.webServiceUrl(params), function(data) {
-         this.processServiceResponce(data, 'Image &quot;' + data.caption + '&quot; updated');
+      var url = gmc.ui.webServiceUrl(params);
+      console.log(url);
+      $.getJSON(url, function(data) {
+         console.log(data);
+         gmc.ui.processServiceResponce(data, 'Image &quot;' + data.caption + '&quot; updated');
          });
       },
    updateSettingsWebsite: function(field, value) {
@@ -68,13 +71,14 @@ gmc.ui = {
          field:  field,
          value:  value
          };
-      console.log('f:' + field, 'v:' + value, 'url:' + this.webServiceUrl(params));
-      $.getJSON(this.webServiceUrl(params), function(data) {
-         this.processServiceResponce(data, 'Gallery settings updated');
+      var url = gmc.ui.webServiceUrl(params);
+      console.log(url);
+      $.getJSON(url, function(data) {
+         gmc.ui.processServiceResponce(data, 'Gallery settings updated');
          });
       },
    menuBarActionAllowed: function(action, page, loc, len, show) {
-      return (action === 'up'   && (loc > 1)) ||
+      return (action === 'up' && (loc > 1)) ||
          (action === 'down' && (loc !== len - 1 && page !== 'gallery')) ||
          (action === 'show' && (!show)) ||
          (action === 'hide' && (show && page !== 'gallery')) ||
@@ -84,21 +88,23 @@ gmc.ui = {
    configureMenuBarButtonsSinglePage: function(data, loc) {
       var buttons = $('#settings-menu-bar buttons');
       function cfg(action) {
-         var allowed = this.menuBarActionAllowed(action,
-            this.menuBarPages[loc], loc, 3, data[loc].show);
-         buttons.find('.' + action + '[name=' + this.menuBarPages[loc] + ']')
+         var allowed = gmc.ui.menuBarActionAllowed(action,
+            gmc.ui.menuBarPages[loc], loc, 3, data[loc].show);
+         buttons.find('.' + action + '[name=' + gmc.ui.menuBarPages[loc] + ']')
             .attr('disabled', allowed);
          }
       if (buttons.length)
-         this.menuBarButtons.forEach(cfg);
+         gmc.ui.menuBarButtons.forEach(cfg);
       },
    configureMenuBarButtons: function() {
       var params = {
          action: 'menu-bar'
          };
-      $.getJSON(this.webServiceUrl(params), function(data) {
-         this.menuBarPages.forEach(function(page, loc) {
-            this.configureMenuBarButtonsSinglePage(data, loc);
+      var url = gmc.ui.webServiceUrl(params);
+      console.log(url);
+      $.getJSON(url, function(data) {
+         gmc.ui.menuBarPages.forEach(function(page, loc) {
+            gmc.ui.configureMenuBarButtonsSinglePage(data, loc);
             });
          });
       },
@@ -109,12 +115,12 @@ gmc.ui = {
          task:   task,
          value:  value
          };
-      console.log('p:' + page, 't:' + task, 'v:' + value,
-         'url:' + this.webServiceUrl(params));
-      $.getJSON(this.webServiceUrl(params), function(data) {
-         this.processServiceResponce(data, 'Gallery menu bar updated');
-         var loc = this.menuBarPages.indexOf(page);
-         this.configureMenuBarButtonsSinglePage(data, loc);
+      var url = gmc.ui.webServiceUrl(params);
+      console.log(url);
+      $.getJSON(url, function(data) {
+         gmc.ui.processServiceResponce(data, 'Gallery menu bar updated');
+         var loc = gmc.ui.menuBarPages.indexOf(page);
+         gmc.ui.configureMenuBarButtonsSinglePage(data, loc);
          });
       },
    setupActions: function() {
@@ -122,48 +128,48 @@ gmc.ui = {
          if(event.keyCode === 13)
            $('.login button').click();
          });
-      $('#create-account').click(function() { this.doLogin(true); });
-      $('#do-login').click(function() { this.doLogin(false); });
+
       $('.portfolio-display').change(function() {
-         this.updatePortfolio($(this).attr('name'), 'display', $(this).is(':checked'));
+         gmc.ui.updatePortfolio($(this).attr('name'), 'display', $(this).is(':checked'));
          });
       $('.portfolio-caption').blur(function() {
-         this.updatePortfolio($(this).attr('name'), 'caption', $(this).val());
+         gmc.ui.updatePortfolio($(this).attr('name'), 'caption', $(this).val());
          });
       $('.portfolio-description').blur(function() {
-         this.updatePortfolio($(this).attr('name'), 'description', $(this).val());
+         gmc.ui.updatePortfolio($(this).attr('name'), 'description', $(this).val());
          });
       $('.portfolio-badge').blur(function() {
-         this.updatePortfolio($(this).attr('name'), 'badge', $(this).val());
+         gmc.ui.updatePortfolio($(this).attr('name'), 'badge', $(this).val());
          });
+
       $('.portfolio-move-up').blur(function() {
-         this.updatePortfolio($(this).attr('name'), 'move', 'up');
+         gmc.ui.updatePortfolio($(this).attr('name'), 'move', 'up');
          });
       $('.portfolio-move-down').blur(function() {
-         this.updatePortfolio($(this).attr('name'), 'move', 'down');
+         gmc.ui.updatePortfolio($(this).attr('name'), 'move', 'down');
          });
       $('.portfolio-delete').blur(function() {
-         this.updatePortfolio($(this).attr('name'), 'command', 'delete');
+         gmc.ui.updatePortfolio($(this).attr('name'), 'command', 'delete');
          });
       $('#settings-website input[type=text]').blur(function() {
-         this.updateSettingsWebsite($(this).attr('name'), $(this).val());
+         gmc.ui.updateSettingsWebsite($(this).attr('name'), $(this).val());
          });
       $('#settings-website select').change(function() {
-         this.updateSettingsWebsite($(this).attr('name'),
+         gmc.ui.updateSettingsWebsite($(this).attr('name'),
             $(this).find('option:selected').text());
          });
       $('#settings-website input[type=checkbox]').change(function() {
-         this.updateSettingsWebsite($(this).attr('name'), $(this).is(':checked'));
+         gmc.ui.updateSettingsWebsite($(this).attr('name'), $(this).is(':checked'));
          });
       $('#settings-menu-bar input[type=text]').blur(function() {
-         this.updateSettingsMenuBar($(this).attr('name'), 'save', $(this).val());
+         gmc.ui.updateSettingsMenuBar($(this).attr('name'), 'save', $(this).val());
          });
       $('#settings-menu-bar button').click(function() {
-         this.updateSettingsMenuBar($(this).attr('name'), $(this).attr('class'));
+         gmc.ui.updateSettingsMenuBar($(this).attr('name'), $(this).attr('class'));
          });
-      $('#change-password').click(function() { this.changePassword(); });
+      $('#change-password').click(function() { gmc.user.changePassword(); });
       $('#create-account').click(function() {
-         this.confirmCreateAccount($('#username').value);
+         gmc.user.confirmCreateAccount($('#username').val());
          });
       }
    };
--- src/gallery/console/js/user-auth.js
+++ src/gallery/console/js/user-auth.js
@@ -22,21 +22,22 @@ gmc.user = {
          valid = true;
       return valid;
       },
-   doLogin: function(validate) {
-      var uname = $('#username').value;
-      var pass =  $('#password').value;
+   login: function(elem) {
+      var validate = elem.data().action == 'create-account';
+      var uname = $('#username').val();
+      var pass =  $('#password').val();
       var hash =  Sha1.hash(pass + gmc.user.salt);
-      $('#submit-username').value = uname;
-      $('#submit-hash').value = hash;
-      if (!validate || this.validateAccountRules(uname, pass, $('#password2').value))
+      $('#submit-username').val(uname);
+      $('#submit-hash').val(hash);
+      if (!validate || gmc.user.validateAccountRules(uname, pass, $('#password2').val()))
          $('#submit-login').submit();
       },
    changePassword: function() {
-      var pass =   $('#password').value;
-      var pass2 =  $('#password2').value;
+      var pass =   $('#password').val();
+      var pass2 =  $('#password2').val();
       var hash = Sha1.hash(pass + gmc.user.salt);
-      $('#submit-hash').value = hash;
-      if (this.validateAccountRules('ignore', pass, pass2))
+      $('#submit-hash').val(hash);
+      if (gmc.user.validateAccountRules('ignore', pass, pass2))
          $('#submit-change-password').submit();
       },
    confirmResetPassword: function(uname) {
@@ -54,13 +55,13 @@ gmc.user = {
       return false;
       },
    confirmCreateAccount: function(uname) {
-      var pass = this.randomPassword();
+      var pass = gmc.user.randomPassword();
       var hash = Sha1.hash(pass + gmc.user.salt);
       var msg = 'You are about to create the following user account.\n\tUsername: ' +
          uname + '\n\tPassword: ' + pass +
          '\n(Hint: You can select and copy the above password.)\n\nContinue?';
-      $('#submit-new-username').value = uname;
-      $('#submit-new-hash').value = hash;
+      $('#submit-new-username').val(uname);
+      $('#submit-new-hash').val(hash);
       if (uname.length === 0)
          alert('Username cannot be blank.');
       else if (confirm(msg))
