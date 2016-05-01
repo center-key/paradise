472a3a45b44c333136631c0950949b0b6d77a2a3
Dem Pilafian <dem@centerkey.com>
2016-05-01 10:56:41 -0700

    Next release

--- src/gallery/console/php/library.php
+++ src/gallery/console/php/library.php
@@ -7,7 +7,7 @@
 // Library
 // Constants and general utilities
 
-$version =    "v0.1.7";
+$version =    "v0.1.8";
 $dataFolder = str_replace("console/php", "~data~", __DIR__);
 
 date_default_timezone_set("UTC");
