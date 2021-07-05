7cabcc2fc07e32184fa9c9fd8f7108b245a64b7d
dpilafian <dem@centerkey.com>
2021-07-04 21:47:45 -0700

    Cleaner og:title and richer og:description

--- src/gallery/image/one.php
+++ src/gallery/image/one.php
@@ -11,16 +11,18 @@
 <meta name=viewport                   content="width=device-width, initial-scale=1">
 <meta name=apple-mobile-web-app-title content="<?=$imageInfo->caption?>">
 <meta name=twitter:card               content="summary_large_image">
-<meta name=twitter:title              content="<?=$settings->title?> - <?=$imageInfo->caption?>">
-<meta name=twitter:description        content="<?=$imageInfo->description?>">
-<meta property=og:title               content="<?=$settings->title?> - <?=$imageInfo->caption?>">
-<meta property=og:description         content="<?=$settings->subtitle?> - <?=$imageInfo->description?>">
+<meta name=twitter:title              content="<?=$imageInfo->caption?>">
+<meta name=twitter:description        content="<?=$imageInfo->description?> - <?=$settings->title?> - <?=$settings->subtitle?>">
+<meta property=og:title               content="<?=$imageInfo->caption?>">
+<meta property=og:description         content="<?=$imageInfo->description?> - <?=$settings->title?> - <?=$settings->subtitle?>">
 <meta property=og:type                content="website">
 <meta property=og:image               content="<?=$imageInfo->urlLarge?>">
 <meta property=og:image:alt           content="<?=$imageInfo->caption?>">
 <title><?=$imageInfo->caption?> &bull; <?=$settings->title?></title>
 <link rel=icon             href=https://centerkey.com/paradise/graphics/bookmark.png>
 <link rel=apple-touch-icon href=<?=$imageInfo->urlSmall?>>
+<link rel=preconnect       href=https://fonts.googleapis.com>
+<link rel=preconnect       href=https://fonts.gstatic.com crossorigin>
 <link rel=stylesheet       href="<?=$values->titleFontUrl?>">
 <link rel=stylesheet       href=https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@@@pkg.cdnVersion.fontAwesome/css/all.min.css>
 <link rel=stylesheet       href=https://cdn.jsdelivr.net/npm/magnific-popup@@@pkg.cdnVersion.magnificPopup/dist/magnific-popup.css>
