43d7dec6694510f203402a10edeaf20359795f4d
dpilafian <dem@centerkey.com>
2022-07-14 22:19:14 -0700

    On fonts page move script tags into head

--- src/gallery/console/fonts.php
+++ src/gallery/console/fonts.php
@@ -6,25 +6,36 @@
 <!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
 <html lang=en>
 <head>
-<meta charset=utf-8>
-<meta name=viewport                   content="width=device-width, initial-scale=1">
-<meta name=robots                     content="index, follow">
-<meta name=description                content="List of fonts availabile for the Paradise PHP Photo Gallery title text">
-<meta name=apple-mobile-web-app-title content="Fonts">
-<title>Fonts Sampler &bull; Paradise</title>
-<link rel=icon             href=https://centerkey.com/paradise/graphics/bookmark.png>
-<link rel=apple-touch-icon href=https://centerkey.com/paradise/graphics/mobile-home-screen.png>
-<link rel=preconnect       href=https://fonts.googleapis.com>
-<link rel=preconnect       href=https://fonts.gstatic.com crossorigin>
-<link rel=stylesheet       href=https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@@@pkg.cdnVersion.fontAwesome/css/all.min.css>
-<link rel=stylesheet       href=https://cdn.jsdelivr.net/npm/dna.js@@@pkg.cdnVersion.dnajs/dist/dna.css>
-<link rel=stylesheet       href=https://cdn.jsdelivr.net/npm/web-ignition@@@pkg.cdnVersion.webIgnition/dist/reset.min.css>
-<link rel=stylesheet       href=../paradise.min.css>
-<link rel=stylesheet       href=../~data~/custom-style.css>
-<style>
-   table.font-sampler tbody tr td { vertical-align: middle; text-align: left; padding: 10px; }
-   table.font-sampler tbody tr td:last-child { width: 100%; }
-</style>
+   <meta charset=utf-8>
+   <meta name=viewport                   content="width=device-width, initial-scale=1">
+   <meta name=robots                     content="index, follow">
+   <meta name=description                content="List of fonts availabile for the Paradise PHP Photo Gallery title text">
+   <meta name=apple-mobile-web-app-title content="Fonts">
+   <title>Fonts Sampler &bull; Paradise</title>
+   <link rel=icon             href=https://centerkey.com/paradise/graphics/bookmark.png>
+   <link rel=apple-touch-icon href=https://centerkey.com/paradise/graphics/mobile-home-screen.png>
+   <link rel=preconnect       href=https://fonts.googleapis.com>
+   <link rel=preconnect       href=https://fonts.gstatic.com crossorigin>
+   <link rel=stylesheet       href=https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@@@pkg.cdnVersion.fontAwesome/css/all.min.css>
+   <link rel=stylesheet       href=https://cdn.jsdelivr.net/npm/dna.js@@@pkg.cdnVersion.dnajs/dist/dna.css>
+   <link rel=stylesheet       href=https://cdn.jsdelivr.net/npm/web-ignition@@@pkg.cdnVersion.webIgnition/dist/reset.min.css>
+   <link rel=stylesheet       href=../paradise.min.css>
+   <link rel=stylesheet       href=../~data~/custom-style.css>
+   <style>
+      table.font-sampler tbody tr td { vertical-align: middle; text-align: left; padding: 10px; }
+      table.font-sampler tbody tr td:last-child { width: 100%; }
+   </style>
+   <script defer src=https://ajax.googleapis.com/ajax/libs/webfont/1.6/webfont.js></script>
+   <script defer src=https://cdn.jsdelivr.net/npm/jquery@@@pkg.cdnVersion.jQuery/dist/jquery.min.js></script>
+   <script defer src=https://cdn.jsdelivr.net/npm/dna.js@@@pkg.cdnVersion.dnajs/dist/dna.min.js></script>
+   <script>window.clientData = <?=appClientData()?>;</script>
+   <script data-on-load=startup>
+      const startup = () => {
+         WebFont.load({ google: { families: clientData.fonts } });
+         $('#font-row h1').css({ fontSize: clientData.titleSize }).text(clientData.title);
+         dna.clone('font-row', clientData.fonts);
+         };
+   </script>
 </head>
 <body>
 
@@ -48,14 +59,5 @@
    <a href=https://fonts.google.com class=external-site>fonts.google.com</a>
 </footer>
 
-<script src=https://ajax.googleapis.com/ajax/libs/webfont/1.6/webfont.js></script>
-<script src=https://cdn.jsdelivr.net/npm/jquery@@@pkg.cdnVersion.jQuery/dist/jquery.min.js></script>
-<script src=https://cdn.jsdelivr.net/npm/dna.js@@@pkg.cdnVersion.dnajs/dist/dna.min.js></script>
-<script>window.clientData = <?=appClientData()?>;</script>
-<script>
-   WebFont.load({ google: { families: clientData.fonts } });
-   $('#font-row h1').css({ fontSize: clientData.titleSize }).text(clientData.title);
-   dna.clone('font-row', clientData.fonts);
-</script>
 </body>
 </html>
