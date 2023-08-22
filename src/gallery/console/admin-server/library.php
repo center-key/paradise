bb0d576657df681df17d10c004a1e938242d6749
dpilafian <dem@centerkey.com>
2023-08-22 01:18:04 -0700

    More granular build tasks

--- src/gallery/console/admin-server/library.php
+++ src/gallery/console/admin-server/library.php
@@ -313,7 +313,7 @@ function refreshSiteMap($gallery) {
    // Update the sitemap.xml file based on the latest gallery data.
    global $siteMapFile;
    $xml = array(
-      '<?xml version="1.0" encoding="UTF-8"?>',
+      '<?xml version="1.0" encoding="UTF-8" ?>',
       '<urlset',
       '   xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"',
       '   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"',
